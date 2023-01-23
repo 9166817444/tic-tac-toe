c9e8a514457f088929dfe86e921cf83fe58c242d
himanshu mishra <ansh.hm1@gmail.com>
2023-01-23 17:19:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GameService } from './service/game.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic-tac-toe';
+
+  constructor(public gameService: GameService) {}
+  
 }
